faa97edc33c2256b950fa36942adb5dbfbe59da7
Nguyễn Quỳnh <thuynguyetquynh@gmail.com>
2024-05-12 10:20:24 +0700

    improve speed

--- read_cameras.py
+++ read_cameras.py
@@ -2,97 +2,6 @@ import os
 import threading
 from record_video import VideoRecorder
 from typing import List
-import glob
-import shutil
-import time
-from tensorflow import keras
-import cv2
-import tensorflow as tf
-from multiprocessing import Process
-
-model_directory = os.path.join("model", '2303_model.h5')
-model = keras.models.load_model(model_directory, compile=False)
-
-def max_consecutive_true(input_list):
-    max_length = 0
-    current_length = 0
-
-    for value in input_list:
-        if value:
-            # If the value is True, increase the current length
-            current_length += 1
-            # Update the max length if the current length is greater
-            max_length = max(max_length, current_length)
-        else:
-            # Reset the current length if the value is False
-            current_length = 0
-
-    return max_length
-
-def detect_fall_each_video(video_path):
-  vid = cv2.VideoCapture(video_path)
-  success, frame = vid.read()
-  if not success:
-      return -1
-  isFall = 0
-  fallCount = 0
-  fallDetectFrames = []
-  while success:
-      try:
-          frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-      except:
-          return -1
-      frame = cv2.resize(frame, (32, 32))
-      frame = tf.keras.utils.img_to_array(frame)
-      frame = tf.expand_dims(frame, 0)
-      detect = model.predict(frame, verbose=0).argmax()
-      fallDetectFrames.append(detect)
-      if detect:
-        fallCount+=1
-        if fallCount>12 and max_consecutive_true(fallDetectFrames)>6:
-          isFall = 1
-          break
-      success, frame = vid.read()
-  return isFall
-
-
-def get_video_paths(output_folder):
-    video_paths = []
-    video_patterns = ['*.mp4', '*.avi', '*.mov']
-    for root, dirs, files in os.walk(output_folder):
-        for pattern in video_patterns:
-            for filename in glob.glob(os.path.join(root, pattern)):
-                video_paths.append(filename)
-    video_paths.sort(key=lambda x: os.path.getctime(x))
-    return video_paths
-
-
-def detect_fall(output_folder: str, classified_folder: str):
-    call_times = 0
-    while True:
-        videos_paths = get_video_paths(output_folder)
-        if len(videos_paths) == 0:
-            time.sleep(10)
-            call_times += 1
-            if call_times == 10:  # Call this function 10 times but no videos
-                break
-        else:
-            call_times = 0
-            for video_path in videos_paths:
-                new_video_path = os.path.relpath(video_path, output_folder)
-                new_video_path = new_video_path.replace(
-                    " ", "").replace("/", "-")
-                isFall = detect_fall_each_video(video_path)
-                if isFall == -1:
-                    continue
-                if isFall:
-                    new_video_path = os.path.join(
-                        classified_folder, "label_1", new_video_path)
-                    shutil.move(video_path, new_video_path)
-                else:
-                    new_video_path = os.path.join(
-                        classified_folder, "label_0", new_video_path)
-                    shutil.move(video_path, new_video_path)
 
 
 def create_folders(out_folder: str, number_of_sub_folder: int = 0) -> List[str]:
@@ -110,6 +19,7 @@ def create_folders(out_folder: str, number_of_sub_folder: int = 0) -> List[str]:
 
 if __name__ == "__main__":
     output_folder = "output_folder"
+    fall_folder = "fall_folder"
     camera_ids = [0, 1]
     n_camera = 2
     video_duration = 10
@@ -118,27 +28,20 @@ if __name__ == "__main__":
     # Create output folder for each camera.
     output_folder_list = create_folders(
         output_folder, number_of_sub_folder=n_camera)
-
-    # Create output for model
-    classified_folder = "classified_folder"
-    os.makedirs(classified_folder, exist_ok=True)
-    os.makedirs(os.path.join(classified_folder, "label_0"), exist_ok=True)
-    os.makedirs(os.path.join(classified_folder, "label_1"), exist_ok=True)
+    fall_folder_list = create_folders(
+        fall_folder, number_of_sub_folder=n_camera)
 
     # Create Video Writers.
-    video_recorder_list = [VideoRecorder(camera_idx=camera_ids[i], output_path=output_folder_list[i],
+    video_recorder_list = [VideoRecorder(camera_idx=camera_ids[i], output_path=output_folder_list[i], fall_path=fall_folder_list[i],
                                          video_duration=video_duration, overlap_time=overlap_time)
                            for i in range(n_camera)]
     # Create Threads.
     thread_list = [threading.Thread(target=video_recorder_list[i].start_recording)
                    for i in range(n_camera)]
+
     # Start Running.
     for thread in thread_list:
         thread.start()
-    classify_process = Process(target=detect_fall, args=(
-        output_folder, classified_folder))
-    classify_process.start()
     # Stop.
     for thread in thread_list:
         thread.join()
-    classify_process.join()
--- record_video.py
+++ record_video.py
@@ -3,20 +3,29 @@ import time
 import os
 import math
 from datetime import datetime, timedelta
+import tensorflow as tf
+from tensorflow import keras
 
 
 class VideoRecorder:
     #  Read and save realtime data from camera.
-    def __init__(self, output_path: str, camera_idx: int = 0,
-                 video_duration: int = 10, overlap_time: int = 7) -> None:
+    def __init__(self, output_path: str, fall_path: str, camera_idx: int = 0,
+                 video_duration: int = 10, overlap_time: int = 3) -> None:
         self.video_capture: cv2.VideoCapture = cv2.VideoCapture(camera_idx)
-        self.frame_width: int = int(self.video_capture.get(cv2.CAP_PROP_FRAME_WIDTH))
-        self.frame_height: int = int(self.video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        self.frame_width: int = int(
+            self.video_capture.get(cv2.CAP_PROP_FRAME_WIDTH))
+        self.frame_height: int = int(
+            self.video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
         self.fourcc: cv2.Videowriter_fourcc = cv2.VideoWriter_fourcc(*'mp4v')
         self.output_path: str = output_path
+        self.fall_path: str = fall_path
         self.video_duration: int = video_duration
         self.overlap_time: int = overlap_time  # Overlap time of each two video
         self.fps: int = self.set_fps()  # Frames per second
+        self.total_frames = int(self.video_duration * self.fps)
+        self.overlap_frames = int(overlap_time * self.fps)
+        model_directory = os.path.join("model", '2303_model.h5')
+        self.model = keras.models.load_model(model_directory, compile=False)
 
     def set_fps(self) -> int:
         """Set fps for camera."""
@@ -24,7 +33,8 @@ class VideoRecorder:
             frame_rate = self.video_capture.get(cv2.CAP_PROP_FPS)
             return int(frame_rate)
         except ValueError:
-            self.video_capture.set(cv2.CAP_PROP_FPS, 12)  # set default FPS values
+            # set default FPS values
+            self.video_capture.set(cv2.CAP_PROP_FPS, 12)
             return 12
 
     def start_recording(self) -> None:
@@ -35,60 +45,86 @@ class VideoRecorder:
 
         time.sleep(1)  # wait 1s for start camera
 
-        # Number of Video Writer needed
-        n_writers = math.ceil(self.video_duration / (self.video_duration - self.overlap_time))
+        writers = []
+        frame_count = 0
 
-        # Create time range for each Video Writer
-        lower = timedelta(seconds=0)
-        upper = timedelta(seconds=self.video_duration)
-        step_time = timedelta(seconds=self.video_duration - self.overlap_time)
-        time_ranges = [(lower + i * step_time, upper + i * step_time) for i in range(n_writers)]
+        isFall = 0
+        saved_frames = []
+        fall_writer = None
 
-        # Create Video Writers
-        start_time = datetime.now()
-        writer_list = [self._start_video_writer(start_time + i * step_time) for i in range(n_writers)]
-
-        # Start Recording
         while True:
             ret, frame = self.video_capture.read()
             if not ret:
-                continue
+                break
+            previousIsFall = isFall
+            isFall = self.checkFall(frame)
+            if isFall:
+                saved_frames.append(frame)
+                if not previousIsFall:
+                    start_fall = datetime.now()
 
-            delta_time = datetime.now() - start_time  # Current time
-            for i in range(n_writers):  # loop each time range
-                if ((delta_time >= time_ranges[i][0]) and
-                        (delta_time <= time_ranges[i][1])):  # check if current time in time range.
-                    writer_list[i].write(frame)
-                elif delta_time > time_ranges[i][1]:  # if not, update Video Writer and time range.
-                    writer_list.pop(i).release()
-                    writer_list.insert(i, self._start_video_writer(start_time + n_writers
-                                                                   * step_time + time_ranges[i][0]))
-                    new_range = (time_ranges[i][0] + n_writers * step_time,
-                                 time_ranges[i][1] + n_writers * step_time)
-                    time_ranges.pop(i)
-                    time_ranges.insert(i, new_range)
+            if not isFall:
+                if len(saved_frames) > self.fps*3-2:
+                    fall_writer = self._start_video_writer(
+                        start_fall, self.fall_path)
+                    for saved_frame in saved_frames:
+                        fall_writer.write(saved_frame)
+                    fall_writer.release()
+                    saved_frames = []
+                    fall_writer = None
 
-            if cv2.waitKey(1) & 0xFF == ord('q'):  # Press 'Q' to stop recording
-                for writer in writer_list:
-                    writer.release()
+            # Start a new writer when the remaining frames equal the overlap frames
+            if frame_count == 0 or frame_count % (self.total_frames - self.overlap_frames) == 0:
+                new_writer = self._start_video_writer(datetime.now())
+                writers.append((new_writer, frame_count + self.total_frames))
+
+            # Remove writers whose range has ended
+            if writers and frame_count >= writers[0][1]:
+                writers.pop(0)[0].release()
+
+            # Write the frame to all active writers
+            if fall_writer:
+                fall_writer.write(frame)
+            for w, _ in writers:
+                w.write(frame)
+
+            frame_count += 1
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 
-    def create_file_path(self, start_time: datetime) -> str:
+    def create_file_path(self, start_time: datetime, classified_output) -> str:
         """Create file path for video."""
         end_time = start_time + timedelta(seconds=self.video_duration)
         start_timestamp = start_time.strftime("%S-%M-%H-%d-%m-%y")
         end_timestamp = end_time.strftime("%S-%M-%H-%d-%m-%y")
-        return os.path.join(self.output_path, f"{start_timestamp}__{end_timestamp}_.mp4")
+        if not classified_output:
+            return os.path.join(self.output_path, f"{start_timestamp}__{end_timestamp}_.mp4")
+        else:
+            return os.path.join(classified_output, f"{start_timestamp}__{end_timestamp}_.mp4")
 
-    def _start_video_writer(self, start_time: datetime) -> cv2.VideoWriter:
+    def _start_video_writer(self, start_time: datetime, classified_output=None) -> cv2.VideoWriter:
         """ Create cv2.VideoWriter instance for writing video."""
-        file_path = self.create_file_path(start_time)
+        file_path = self.create_file_path(start_time, classified_output)
         return cv2.VideoWriter(file_path, self.fourcc, self.fps, (self.frame_width, self.frame_height))
 
     def cleanup(self) -> None:
         print("Release Resources.")
         self.video_capture.release()
         cv2.destroyAllWindows()
+
+    def checkFall(self, frame):
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        frame = cv2.resize(frame, (32, 32))
+        frame = tf.keras.utils.img_to_array(frame)
+        frame = tf.expand_dims(frame, 0)
+        frame = frame/255.
+        detect = self.model.predict(frame, verbose=0)[0]
+        label = detect.argmax()
+        proba = max(detect)
+        if label < 4 and proba > 0.5:
+            return 1
+        return 0
 
     def __del__(self) -> None:
         if self.video_capture.isOpened():
@@ -99,6 +135,9 @@ if __name__ == "__main__":
     # Create a folder saving collected data
     output_folder = "output_folder"
     os.makedirs(output_folder, exist_ok=True)
+
+    fall_folder = "fall_folder"
+    os.makedirs(fall_folder, exist_ok=True)
 
     recorder = VideoRecorder(output_path=output_folder)
     recorder.start_recording()
